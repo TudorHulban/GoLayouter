c713d3f6fa2c6abeb7df673b9a068a900797a029
cezar <njroffic@gmail.com>
2022-11-28 20:22:48 +0200

    objects tests updated

--- domain/objects/obj_entry_test.go
+++ domain/objects/obj_entry_test.go
@@ -25,21 +25,22 @@ func TestWriteObjectsToFile(t *testing.T) {
 		fileInput   string
 		fileOutput  string
 	}{
-		{"2 levels", "../test_cases/folder_c1", "../test_cases/folder_c1_results"},
-		{"3 levels", "../test_cases/folder_c2", "../test_cases/folder_c2_results"},
-		{"3 levels with going back", "../test_cases/folder_c3", "../test_cases/folder_c3_results"},
-		{"invalid path", "../test_cases/folder_c4", "../test_cases/folder_c4_results"},
-		{"file without packages", "../test_cases/folder_c5", "../test_cases/folder_c5_results"},
-		{"files + paths + packages", "../test_cases/folder_c6", "../test_cases/folder_c6_results"},
+		{"2 levels", "folder_c1", "folder_c1_results"},
+		{"3 levels", "folder_c2", "folder_c2_results"},
+		{"3 levels with going back", "folder_c3", "folder_c3_results"},
+		{"invalid path", "folder_c4", "folder_c4_results"},
+		{"file without packages", "folder_c5", "folder_c5_results"},
+		{"files + paths + packages", "folder_c6", "folder_c6_results"},
+		{"small test with packages", "folder_c7", "folder_c7_results"},
 	}
 
 	for _, tc := range testCases {
 		t.Run(tc.description, func(t *testing.T) {
-			content, errRe := helpers.ReadFile("../" + tc.fileInput)
+			content, errRe := helpers.ReadFile("../../test_cases/" + tc.fileInput)
 			require.NoError(t, errRe)
 			entries := NewEntries(content).Parse()
 
-			output, errRead := helpers.ReadFile("../" + tc.fileOutput)
+			output, errRead := helpers.ReadFile("../../test_cases/" + tc.fileOutput)
 			require.NoError(t, errRead)
 
 			assert.Equal(t, output, entries, "should be equal")
