067193f66db930c323fe1cfeacc2c6885870e211
cezar <njroffic@gmail.com>
2022-12-25 19:25:11 +0200

    new object

--- domain/objects/item/obj_item_test.go
+++ domain/objects/item/obj_item_test.go
@@ -1,35 +1,3 @@
 package item
 
-import (
-	"log"
-	"testing"
-
-	"github.com/TudorHulban/GoLayouter/app/helpers"
-	"github.com/TudorHulban/GoLayouter/domain/objects/entry"
-	"github.com/stretchr/testify/require"
-)
-
-const _TestCases = "../../test_cases/"
-
-func TestParseToItems(t *testing.T) {
-	testCases := []struct {
-		description string
-		fileInput   string
-		fileOutput  string
-	}{
-		{"files + paths + packages", "folder_c6", "folder_c6_results"},
-	}
-
-	for _, tc := range testCases {
-		t.Run(tc.description, func(t *testing.T) {
-			content, errRe := helpers.ReadFile(_TestCases + tc.fileInput)
-			require.NoError(t, errRe)
-			entries := entry.NewEntries(content).Parse()
-
-			for _, item := range entries {
-				log.Print(item.path, " ", item.kind)
-			}
-
-		})
-	}
-}
+const _pathInput = "../../test_cases/folder_c6"
