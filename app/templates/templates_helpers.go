64c1e32d51202d17197e30289e16b44be15ac383
cezar <njroffic@gmail.com>
2023-01-02 18:44:16 +0200

    spacing the templates

--- app/templates/templates_helpers.go
+++ app/templates/templates_helpers.go
@@ -3,26 +3,10 @@ package templates
 import (
 	"html/template"
 	"io"
-	"os"
 )
 
-func renderToPath(templateFilePath, renderToPath string, model any) error {
-	file, errCreate := os.Create(renderToPath)
-	if errCreate != nil {
-		return errCreate
-	}
-	defer file.Close()
-
-	t, errParse := template.ParseGlob(templateFilePath)
-	if errParse != nil {
-		return errParse
-	}
-
-	return t.Execute(file, model)
-}
-
-func RanderTo(templateFilePath string, w io.Writer, model any) error {
-	t, errParse := template.ParseGlob(templateFilePath)
+func RenderTo(w io.Writer, templateFilePath string, model any) error {
+	t, errParse := template.ParseFiles(templateFilePath)
 	if errParse != nil {
 		return errParse
 	}
--- app/templates/templates_test.go
+++ app/templates/templates_test.go
@@ -5,23 +5,50 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/require"
+	"github.com/udhos/equalfile"
 )
 
-const MainTemplateFilePath = "templates/test"
-const randerToPath = "templates/output"
+const from = "input/"
+const to = "test/"
 
-type File struct {
+type Model struct {
 	FileName   string
+	ObjectName string
 	Package    string
-	IsTestFile bool
+}
+
+var m = Model{
+	FileName:   "main.go",
+	ObjectName: "Main",
+	Package:    "main",
 }
 
 func TestRenderToPath(t *testing.T) {
-	f := File{
-		FileName:   "Foo",
-		Package:    "test",
-		IsTestFile: true,
+	testCases := []struct {
+		description string
+		input       string
+		output      string
+	}{
+		{"main template", from + "main", to + "main_output"},
+		{"object template", from + "object", to + "object_output"},
+		{"tdd template", from + "tableDriven", to + "tdd_output"},
+		{"test template", from + "test", to + "test_output"},
 	}
 
-	require.NoError(t, RanderTo(MainTemplateFilePath, os.Stdout, f))
+	for _, tc := range testCases {
+		t.Run(tc.description, func(t *testing.T) {
+			{
+				buffer, errCreate := os.Create("buffer")
+				require.NoError(t, errCreate)
+				require.NoError(t, RenderTo(buffer, tc.input, m))
+
+				cmp := equalfile.New(nil, equalfile.Options{}) // compare using single mode
+				equal, errCompare := cmp.CompareFile(buffer.Name(), tc.output)
+
+				require.NoError(t, errCompare)
+				require.Equal(t, true, equal)
+				require.NoError(t, os.Remove(buffer.Name()))
+			}
+		})
+	}
 }
